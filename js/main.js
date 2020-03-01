ab67b97e59f98094368916be03a5eee5744836a6
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-02-29 21:55:54 -0600

    creando funciones para definir el template de las card para los productos

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let bracelet = document.getElementById('bracelet');
 let notebook = document.getElementById('notebook');
 let miniNotebook = document.getElementById('mini-notebook');
 //Information
-let bracelets = [
+const bracelets = [
     { name: 'Pulsera Mod.1 roja', price: 45 },
     { name: 'Pulsera Mod.2 verde', price: 45 },
     { name: 'Pulsera Mod.3 morada', price: 45 },
@@ -18,23 +18,101 @@ let bracelets = [
     { name: 'Pulsera Mod.8 turquesa', price: 45 }
 ];
 
-let notebooks = [
+const notebooks = [
     { name: 'Libreta Mod.1 Abstract blue', price: 100 },
     { name: 'Libreta Mod.2 Abstract orange', price: 100 },
     { name: 'Libreta Mod.3 Abstract purple', price: 100 },
     { name: 'Libreta Mod.4 Abstract green', price: 100 }
 ];
 
-let miniNotebook = [
+const miniNotebooks = [
     { name: 'Mini libretita Mod.1 espacio', price: 50 },
     { name: 'Mini libretita Mod.2 floral', price: 50 },
     { name: 'Mini libretita Mod.3 metalica', price: 50 },
     { name: 'Mini libretita Mod.4 naturaleza', price: 50 }
-    
 ];
+//HTML
+const braceletsAll = (con) => {
+    let url = `img/bracelets/bracelet${con}.jpeg`;
+    let btn = `btnBracelet${con}`;
+
+    return `<div class="col s3 wow fadeInUp data-wow-delay="3s" data-wow-offset="300" ">
+              <div class="card">
+              <div class="card-image">
+                <img src="${url}">
+                <a id="${btn}" onclick="cart('${bracelet[con-1].name}', '${bracelet[con-1].price}', 
+                    ${URL}', '${con}', '${btn}')" class="btn btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">add</li>
+                </a>
+              </div>
+              <div class="card-content">
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <span class="card-title">${bracelet[con-1].name}</span>
+              <p>Price: ${bracelet[con-1].price}</p>
+               </div>
+            </div>
+        </div>`
+};
+
+const notebooksAll = (con) => {
+    let url = `img/notebooks/notebook${con}.jpeg`;
+    let btn = `btnNotebook${con}`;
+
+    return `<div class="col s3 wow fadeInUp data-wow-delay="3s" data-wow-offset="300" ">
+              <div class="card">
+              <div class="card-image">
+                <img src="${url}">
+                <a id="${btn}" onclick="cart('${notebook[con-1].name}', '${notebook[con-1].price}', 
+                    ${URL}', '${con}', '${btn}')" class="btn btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">add</li>
+                </a>
+              </div>
+              <div class="card-content">
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <span class="card-title">${notebook[con-1].name}</span>
+              <p>Price: ${notebook[con-1].price}</p>
+               </div>
+            </div>
+        </div>`
+};
+
+const miniNotebooksAll = (con) => {
+    let url = `img/mininotebooks/mininotebook${con}.jpeg`;
+    let btn = `btnMiniNotebook${con}`;
+
+    return `<div class="col s3 wow fadeInUp data-wow-delay="3s" data-wow-offset="300" ">
+              <div class="card">
+              <div class="card-image">
+                <img src="${url}">
+                <a id="${btn}" onclick="cart('${mininotebook[con-1].name}', '${mininotebook[con-1].price}', 
+                    ${URL}', '${con}', '${btn}')" class="btn btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">add</li>
+                </a>
+              </div>
+              <div class="card-content">
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <i class="material-icons" style="color:orange">star</li>
+              <span class="card-title">${mininotebook[con-1].name}</span>
+              <p>Price: ${mininotebook[con-1].price}</p>
+               </div>
+            </div>
+        </div>`
+};
 
 
-//CAROUSEL
+
+//CAROUSEL MATERIALIZE CSS
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
@@ -42,5 +120,4 @@ document.addEventListener('DOMContentLoaded', function () {
         indicators: true,
         duration: 100
     });
-    instances.next(1);
 });
