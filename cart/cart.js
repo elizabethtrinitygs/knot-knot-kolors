7ae181c817b8a7b4957c75b09a662e1903741210
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-01 22:37:36 -0600

    creando funcion para imprimir los datos de los productos seleccionados, llamando el seervicio de firebase

--- cart/cart.js
+++ cart/cart.js
@@ -24,5 +24,39 @@ var firebaseConfig = {
                 <td><img style= "width:90px;" src="${products[i].url}"></td>
                 <td>${1}</td>
                 <td>${products[i].price}</td>
-            </tr> `
+            </tr> `;
   }
+  
+  //FORM CART
+document.getElementById('formCart').addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    userName = document.getElementById('userName');
+    userEmail = document.getElementById('userEmail');
+    userSelect = document.getElementById('userSelect');
+
+    let d = new Date();
+    let t = d.getTime();
+    let order = t-300;
+
+    firebase.database().ref('orders').push({
+        id: t+1,
+        order: order,
+        userName: userName.value,
+        payment: userSelect.value,
+        date: d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getMonth.FullYear(),
+        hour: d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(),
+        year: d.getFullYear(),
+        products: JSON.parse(localStorage.getItem("cart")),
+        total: total
+    });
+    swal.fire({
+        position: 'center',
+        type: 'success',
+        title: 'Purchase made successfully!',
+        text: `Your purchase order is: ${order}`,
+        showConfirmButton: true,
+        timer: 50000
+    });
+    clean();
+})
