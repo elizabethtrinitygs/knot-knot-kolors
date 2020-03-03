b86347811093dee9b1d126db1d6882f7e91d98b4
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-02 20:54:21 -0600

    solucionado el problema con firebase y metodos para imprimir la fecha

--- cart/cart.js
+++ cart/cart.js
@@ -10,10 +10,12 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  // Get a reference to the database service
+
 
   //GLOBAL
   const products = JSON.parse(localStorage.getItem('cart'));
-  let carItem = [];
+  let cartItem = [];
   const cart_n = document.getElementById('cart_n');
   const table = document.getElementById('table');
   let total = 0;
@@ -21,11 +23,13 @@ var firebaseConfig = {
   const tableContent = (i) => {
     return `<tr class="">
                 <th scope="row">${i + 1}</th>
-                <td><img style= "width:90px;" src="${products[i].url}"></td>
+                <td><img src="${products[i].url}"></td>
+                <td>${products[i].name}</td>
                 <td>${1}</td>
                 <td>${products[i].price}</td>
             </tr> `;
   }
+  
   
   //FORM CART
 document.getElementById('formCart').addEventListener('submit', (e)=> {
@@ -39,17 +43,20 @@ document.getElementById('formCart').addEventListener('submit', (e)=> {
     let t = d.getTime();
     let order = t-300;
 
+    // let database= firebase.database();
     firebase.database().ref('orders').push({
         id: t+1,
         order: order,
         userName: userName.value,
         payment: userSelect.value,
-        date: d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getMonth.FullYear(),
+        date: d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear(),
         hour: d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(),
         year: d.getFullYear(),
         products: JSON.parse(localStorage.getItem("cart")),
         total: total
     });
+    
+
     swal.fire({
         position: 'center',
         type: 'success',
@@ -58,5 +65,60 @@ document.getElementById('formCart').addEventListener('submit', (e)=> {
         showConfirmButton: true,
         timer: 50000
     });
-    clean(); //video 1:56:00 min
-})
+    clean(); 
+ });
+//CLEAN FUNCTION
+const clean = () => {
+    localStorage.clear();
+    for (let index = 0; index < products.length; index++) {
+        table.innerHTML += tableContent(index);
+        total = total + parseInt(products[index].price);
+    }
+    total= 0;
+
+    table.innerHTML = `
+        <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        </tr>`;
+    document.getElementById('btnBuy').style.display = "none";
+    document.getElementById('btnClean').style.display = "none";
+}
+
+//RENDER
+const render = () => {
+    for (let index = 0; index < products.length; index++) {
+        table.innerHTML += tableContent(index);
+        total = total + parseInt(products[index].price);
+    }
+    table.innerHTML += `
+        <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col">Total $${total}.00</th>
+        </tr>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col">
+            <button id="btnClean" onclick="clean()" class="btn yellow darken-4">
+            Clean Shopping Cart</button>
+            </th>
+            <th scope="col">
+            <button id="btnBuy" href="#modal1" class="modal-trigger waves-effect waves-light btn">
+            Buy</button>
+            </th>
+        </tr>`;
+}
+
+//MODAL MATERIALIZE
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
